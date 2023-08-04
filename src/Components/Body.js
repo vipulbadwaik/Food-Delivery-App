b183de81158619fa9c6e364e444c0f87108e9ca4
vipulbadwaik <vipul.badwaik@gmail.com>
2023-08-04 23:34:10 +0530

    Used hooks 1st time

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,19 +1,30 @@
 import ResCard from "./RestraurantCard";
 import resObj from "../utils/MockData";
+import { useState } from "react";
 
 const Body = () => {
+
+  const [listOfRestraunt,setListOfRestraunt] = useState(resObj);
+    
+ 
+
+
     return (
       <div className="body">
         <div className="filter">
           <button className="filter-btn"
           onClick={()=>{
             console.log("Button Clicked");
+            const filteredList = listOfRestraunt.filter(
+              (res) => res.data.avgRating>4
+            );
+            setListOfRestraunt(filteredList);
           }}
           >Top Rated restaurant</button>
         </div>
         <div className="Restra">
          {
-          resObj.map((restaurant) => (<ResCard key={restaurant.data.id} resData={restaurant}/>))
+          listOfRestraunt.map((restaurant) => (<ResCard key={restaurant.data.id} resData={restaurant}/>))
          }
           
         </div>
